c674365eef4de373a9c7b42a858deee010a8f26c
Camarones <cameron_walther@yahoo.com>
2019-04-22 16:35:17 -0400

    finished up server and html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+    // this route will send us to the survey.html
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(_dirname, "../public/survey.html"));
+    });
+    
+    // and this route will automatically send us to home.html for anything other than survey
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(_dirname, "..public/home.html"));
+    });
+
+}
--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+// boilerplate server code
 var express = require("express");
-var path = require("path");
 
+var app = express();
+
+var PORT = process.env.PORT || 5309;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// give our server the routes in our file tree
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT)
+});
