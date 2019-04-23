45ded9ee3fe1bbee344508ac9ec4b846ab467142
Camarones <cameron_walther@yahoo.com>
2019-04-22 23:12:39 -0400

    finished up compatability comparison, still needs jquery to grab values and the modal to display your new best friend, deployed to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 
 var app = express();
 
-var PORT = process.env.PORT || 5309;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
