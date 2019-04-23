0c3049df29172b77ca7ea9255dd11ed95bf78be5
Camarones <cameron_walther@yahoo.com>
2019-04-22 23:04:04 -0400

    deploying to heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,6 +8,28 @@ module.exports = function(app) {
     })
 
     app.post("api/friends", function(req, res) {
+        
+
+        var newScores = req.body.scores;
+        var topMatch = 0;
+        var scoresArray = [];
+
+        friendData.forEach(function(friend) {
+            var totalDifference = 0;
+            newScores.forEach(function(score) {
+                totalDifference += Math.abs(parseInt(friend.scores) - parseInt(score))
+            })
+            scoresArray.push(totalDifference);
+        })
+
+        scoresArray.forEach(function(match) {
+            if (match <= match[topMatch]) {
+                topMatch = match
+            }
+            var bestFriend = friendData[topMatch];
+            res.json(bestFriend);
+        })
+
         friendData.push(req.body);
     })
 }
