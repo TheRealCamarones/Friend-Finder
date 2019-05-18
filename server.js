f85c89e5436a7729050b7bae54e9b2505602ae98
Camarones <cameron_walther@yahoo.com>
2019-05-18 14:10:53 -0400

    thanks to Patrick for the assist, got it all working now

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("app/public"));
 
 // give our server the routes in our file tree
 require("./app/routing/apiRoutes")(app);
